d8dec328f066a983f92756d9a73437c4a713de2d
Nicolas Alder <nicolasalder@MacBook-Pro-von-Nicolas.local>
2023-02-06 07:54:22 +0100

    test

--- experiments/main.py
+++ experiments/main.py
@@ -223,7 +223,7 @@ def eval(model, loader, mode):
         res = np.mean(
             [
                 #(model(x.to('cuda').round()).argmax(-1) == y.to('cuda')).to(torch.float32).mean().item()
-                ((model(x.to('cuda').round()) == y.to('cuda')).to(torch.float32)).sum().item() / 5
+                ((model(x.to('cuda').round()) == y.to('cuda')).to(torch.float32)).sum().item() / 500
 
                 for x, y in loader
             ]
