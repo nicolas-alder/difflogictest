a0bfc22b59b40f93c5a6d1cc646507db616922f1
Nicolas Alder <nicolasalder@MacBook-Pro-von-Nicolas.local>
2023-02-07 16:28:08 +0100

    test

--- experiments/main.py
+++ experiments/main.py
@@ -25,7 +25,7 @@ BITS_TO_TORCH_FLOATING_POINT_TYPE = {
     64: torch.float64
 }
 def weighted_mse_loss(input, target):
-    weight = torch.tensor(np.power(2, list(range(5))*100).reshape(-1,5))
+    weight = torch.tensor(np.power(2, list(range(5))*100).reshape(-1,5)).to('cuda')
     print(input.size())
     print(target.size())
     print(weight.size())
