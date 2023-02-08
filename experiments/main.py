e557a994ed2e8f898d890c6075ced7c890a0515d
Nicolas Alder <nicolasalder@MacBook-Pro-von-Nicolas.local>
2023-02-08 13:40:52 +0100

    test

--- experiments/main.py
+++ experiments/main.py
@@ -276,13 +276,12 @@ def eval(model, loader, mode):
         model.train(mode=mode)
         if args.dataset == 'custom':
             print(model(x.to('cuda')).round().to(torch.float32).size())
-            res = np.mean(
-            [
+            res = torch.tensor([
                 #(model(x.to('cuda').round()).argmax(-1) == y.to('cuda')).to(torch.float32).mean().item()
                 #((model(x.to('cuda').round()) == y.to('cuda')).to(torch.float32)).sum().item() / 500
-                (torch.tensor(np.power(2, list(range(5)) * 100).reshape(-1, 5)) * model(x.to('cuda')).round().to(torch.float32).cpu()) - (y * torch.tensor(np.power(2, list(range(5)) * 100).reshape(-1, 5)).to(torch.float32))
+                ((torch.tensor(np.power(2, list(range(5)) * 100).reshape(-1, 5)) * model(x.to('cuda')).round().to(torch.float32).cpu()) - (y * torch.tensor(np.power(2, list(range(5)) * 100).reshape(-1, 5)).to(torch.float32))).mean(dim=0)
             for x, y in loader
-            ])
+            ]).mean(dim=0)
         elif args.dataset == 'vectormul':
             res = np.mean(
                 [
