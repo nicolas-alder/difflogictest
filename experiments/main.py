3c8bbbd608beffcc24c60acc5142feebd6489987
Nicolas Alder <nicolasalder@MacBook-Pro-von-Nicolas.local>
2023-02-09 11:34:42 +0100

    test

--- experiments/main.py
+++ experiments/main.py
@@ -284,7 +284,7 @@ def eval(model, loader, mode):
                 #((model(x.to('cuda').round()) == y.to('cuda')).to(torch.float32)).sum().item() / 500
                 (abs((torch.tensor(np.power(2, values * x.size()[0]).reshape(-1, 5)) * model(x.to('cuda')).round().to(torch.float32).cpu()) - (y.cpu() * torch.tensor(np.power(2, values) * x.size()[0]).reshape(-1, 5)).to(torch.float32))).mean(dim=0).numpy()
             for x, y in loader
-            ]).mean(dim=0)
+            ]).mean(dim=1)
         elif args.dataset == 'vectormul':
             res = np.mean(
                 [
